6db13f51fe6d761b04ccd2a89a8f335bbb78c459
Joseph Karasek <joseph.karasek@gmail.com>
2018-04-17 10:10:57 -0700

    add button

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,15 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
+import Button from 'material-ui/Button';
 
 class App extends Component {
     render() {
         return (
             <div>
                 <h1>Hello World</h1>
+                <Button variant="raised" color="primary">
+                    Click Me!
+                </Button>
             </div>
         );
     }
