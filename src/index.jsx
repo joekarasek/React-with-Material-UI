183bc6d538a81ed5ee2229c27e99f1d89a43fd4d
Joseph Karasek <joseph.karasek@gmail.com>
2018-04-17 10:17:39 -0700

    add basic material design card with image

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,8 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
+import Card, { CardActions, CardContent, CardMedia } from 'material-ui/Card';
 import Button from 'material-ui/Button';
+import Typography from 'material-ui/Typography';
 
 class App extends Component {
     render() {
@@ -10,6 +12,29 @@ class App extends Component {
                 <Button variant="raised" color="primary">
                     Click Me!
                 </Button>
+                <Card>
+                    <CardMedia
+                        image="http://placekitten.com/g/200/300"
+                        title="A cute looking cat"
+                    />
+                    <CardContent>
+                        <Typography variant="headline" component="h2">
+                            Beware Cats!
+                        </Typography>
+                        <Typography component="p">
+                            Cats are a widespread species of highly intelligent mammals with mastery of deflective and
+                            disarming practices. Their motives are unclear.
+                        </Typography>
+                    </CardContent>
+                    <CardActions>
+                        <Button size="small" color="primary">
+                            Share
+                        </Button>
+                        <Button size="small" color="primary">
+                            Learn More
+                        </Button>
+                    </CardActions>
+                </Card>
             </div>
         );
     }
